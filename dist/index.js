3a7fc14b5c79b40f49a370a44db87a74d5160cee
Ilknur Ultanir Sari <45620986+iibarbari@users.noreply.github.com>
2023-08-16 18:08:05 +0300

    add bootstrap

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+function updateTheme(mediaMatch) {
+    if (mediaMatch.matches) {
+        document.body.setAttribute("data-bs-theme", "dark");
+    } else {
+        document.body.setAttribute("data-bs-theme", "light");
+    }
+}
+
+// Set the theme based on the user's preference
+if (window.matchMedia) {
+    updateTheme(window.matchMedia('(prefers-color-scheme: dark)'));
+
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(updateTheme);
+}
---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import scss from "rollup-plugin-scss";
+
+export default {
+    input: {
+        index: 'src/index.js',
+    },
+    output: {
+        format: 'es',
+        dir: 'dist',
+    },
+    plugins: [
+        commonjs(),
+        nodeResolve(),
+        scss({
+            fileName: 'styles.css',
+            outputStyle: 'compressed',
+            output: true,
+            dir: 'styles',
+        }),
+    ],
+};
